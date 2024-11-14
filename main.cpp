d0b8b25d1201a20aab89f9def6ba953ea5d8af72
GaelSMora <gaelsans.mora@gmail.com>
2024-11-14 16:46:56 -0600

    Pruebas

--- main.cpp
+++ main.cpp
@@ -12,13 +12,22 @@ int main()
     Alimento ali;
 
     pa.setPrecio(199);
-    cout<<pa.getPrecio()<<endl;
+    cout<<pa.getPrecio()<<"\n"<<endl;
 
     medi.setViaAdministracion("Oral");
+    medi.setNombre("Paracetamol");
+    medi.setTipoMedicamento("Pastilla");
+    cout<<medi.getNombre()<<endl;
     cout<<medi.getViaAdministracion()<<endl;
+    cout<<medi.getTipoMedicamento()<<"\n"<<endl;
 
+    ali.setNombre("Agua");
     ali.setContenidoNutricional("Proteinas 12g");
+    ali.setTipoAlimento("Bebida");
+    cout<<ali.getNombre()<<endl;
     cout<<ali.getContenidoNutricional()<<endl;
+    cout<<ali.getTipoAlimento()<<endl;
+
 
     return 0;
 }
